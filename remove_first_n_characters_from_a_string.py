88193a2414979ebc7ffe9e9d51105459c055760d
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-22 14:39:20 +0800

    Pseudocodes

--- remove_first_n_characters_from_a_string.py
+++ remove_first_n_characters_from_a_string.py
@@ -1,2 +1,8 @@
 # Exercise 4: Remove first n characters from a string
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+#Let user type something
+
+#Let user decide how many characters will be removed from the input
+
+#Display the result
