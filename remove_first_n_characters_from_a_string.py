ab4367077795880a7de654bdd338eea09eefe6a2
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-22 14:41:25 +0800

    Completed user input

--- remove_first_n_characters_from_a_string.py
+++ remove_first_n_characters_from_a_string.py
@@ -2,7 +2,9 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
 #Let user type something
+string_input = input("Please type something: ")
 
 #Let user decide how many characters will be removed from the input
+remove_charac = int(input("How many characters do you want to remove? "))
 
 #Display the result
